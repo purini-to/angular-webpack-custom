e8c2375f131f5cf62af5ddf5276d618d6e91a35a
raizu <raiz.info@gmail.com>
2016-04-03 02:20:16 +0900

    Angular Materialを導入

--- src/app/app.js
+++ src/app/app.js
@@ -1,5 +1,9 @@
 import angular from 'angular';
+import agMaterial from 'angular-material';
+import agAnimate from 'angular-animate';
+import agMdIcons from 'angular-material-icons';
 
+import 'angular-material/angular-material.css';
 import '../style/app.styl';
 
 let app = () => {
@@ -11,15 +15,40 @@ let app = () => {
 };
 
 class AppCtrl {
-  constructor() {
+  constructor($log, $mdSidenav) {
     this.url = 'https://github.com/preboot/angular-webpack';
+    this.$log = $log;
+    this.$mdSidenav = $mdSidenav;
+  }
+
+  toggleSidenav() {
+    this.$mdSidenav('nav').toggle()
+      .then(() => {
+        this.$log.debug('toggle nav is done');
+      });
+  }
+
+  closeSidenav() {
+    this.$mdSidenav('nav').close()
+      .then(() => {
+        this.$log.debug('close nav is done');
+      });
   }
 }
 
+AppCtrl.$inject = ['$log', '$mdSidenav'];
+
 const MODULE_NAME = 'app';
 
-angular.module(MODULE_NAME, [])
+angular.module(MODULE_NAME, [agMaterial, agAnimate, agMdIcons])
   .directive('app', app)
-  .controller('AppCtrl', AppCtrl);
+  .controller('AppCtrl', AppCtrl)
+  .config(['$mdThemingProvider', function($mdThemingProvider) {
+    $mdThemingProvider.theme('myAwesome')
+      .primaryPalette('blue')
+      .accentPalette('cyan')
+      .warnPalette('red');
+    $mdThemingProvider.setDefaultTheme('myAwesome');
+  }]);
 
 export default MODULE_NAME;
